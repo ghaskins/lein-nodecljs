276f7e2d23b15df926135fd0eb0365c8cc270e56
Gregory Haskins <gregory.haskins@gmail.com>
2017-03-26 23:24:38 -0400

    Checkpoint
    Signed-off-by: Gregory Haskins <gregory.haskins@gmail.com>

--- examples/hello/project.clj
+++ examples/hello/project.clj
@@ -6,6 +6,5 @@
   :plugins [[lein-nodecljs "0.1.0-SNAPSHOT"]]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.494"]]
-  :npm {:dependencies [[source-map-support "0.4.0"]
-                       [protobufjs "5.0.1"]]}
+  :npm {:dependencies [[source-map-support "0.4.0"]]}
   :main hello.core)
