a2dfae4484cbdf5c37ba16c1e55b8a3514c0a50a
Gregory Haskins <gregory.haskins@gmail.com>
2017-03-28 10:29:05 -0400

    Make sure the example uses the current development release
    Signed-off-by: Gregory Haskins <gregory.haskins@gmail.com>

--- examples/hello/project.clj
+++ examples/hello/project.clj
@@ -1,9 +1,9 @@
-(defproject hello "0.1.0-SNAPSHOT"
+(defproject hello "1.0.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :plugins [[lein-nodecljs "0.1.0-SNAPSHOT"]]
+  :plugins [[lein-nodecljs "0.2.0-SNAPSHOT"]]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.494"]]
   :npm {:dependencies [[source-map-support "0.4.0"]]}
