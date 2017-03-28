fbb9fa51095fbcbc8022e9565132878229b37488
Gregory Haskins <gregory.haskins@gmail.com>
2017-03-27 23:11:58 -0400

    Add support for compile hook
    Signed-off-by: Gregory Haskins <gregory.haskins@gmail.com>

--- plugin/src/lein_nodecljs/plugin.clj
+++ plugin/src/lein_nodecljs/plugin.clj
@@ -2,10 +2,13 @@
   (:require [lein-nodecljs.core :as core]
             [robert.hooke]
             [leiningen.install]
+            [leiningen.compile]
             [clojure.java.shell :refer [sh]]))
 
 (defn- install-hook
   [f project & args]
+
+  (apply f project args)
 
   (let [workdir (core/compile project)]
 
@@ -15,8 +18,23 @@
       (println (:err retval))
 
       (when (-> retval :exit zero?)
-        (println "Wrote" (:out retval))))))
+        (println (:out retval))))))
+
+(defn- compile-hook
+  [f project & args]
+
+  (apply f project args)
+
+  (let [workdir (core/compile project)]
+
+    (println "[npm] Compiling")
+    (let [retval (sh "npm" "install" :dir (.getCanonicalPath workdir))]
+
+      (println (:err retval))
+
+      (when (-> retval :exit zero?)
+        (println (:out retval))))))
 
 (defn hooks []
-  (robert.hooke/add-hook #'leiningen.install/install
-                         #'install-hook))
+  (robert.hooke/add-hook #'leiningen.install/install install-hook)
+  (robert.hooke/add-hook #'leiningen.compile/compile compile-hook))
