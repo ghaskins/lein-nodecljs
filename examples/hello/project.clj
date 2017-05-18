b311f097002f0da2a3f9fbe34239d5e6405ac606
Gregory Haskins <gregory.haskins@gmail.com>
2017-05-18 16:26:23 -0400

    Prepare for v0.7.0 development
    Signed-off-by: Gregory Haskins <gregory.haskins@gmail.com>

--- examples/hello/project.clj
+++ examples/hello/project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :plugins [[lein-nodecljs "0.6.0"]]
+  :plugins [[lein-nodecljs "0.7.0-SNAPSHOT"]]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.495"]]
   :npm {:dependencies [[source-map-support "0.4.0"]]}
