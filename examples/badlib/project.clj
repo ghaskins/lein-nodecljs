c3fe7df886fbba503b455e987cc0a63d8b2be038
Greg Haskins <gregory.haskins@gmail.com>
2017-09-27 14:26:55 -0400

    Allow file: based dependencies in the npm: section
    Signed-off-by: Greg Haskins <gregory.haskins@gmail.com>

---
+++ examples/badlib/project.clj
@@ -0,0 +1,10 @@
+(defproject hello "1.0.0"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :plugins [[lein-nodecljs "0.11.0-SNAPSHOT"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.9.495"]]
+  :npm {:dependencies [[source-map-support "file:./lib/source-map-support-0.4.0.tgz"]]}
+  :nodecljs {:main hello.core})
--- plugin/src/lein_nodecljs/package.clj
+++ plugin/src/lein_nodecljs/package.clj
@@ -1,6 +1,18 @@
 (ns lein-nodecljs.package
   (:require [clojure.java.io :as io]
+            [me.raynes.fs :as fs]
             [cheshire.core :as json]))
+
+(defn- translate-version [version]
+  (when-let [[_ path] (re-matches #"file:(.*)" version)]
+    (let [file (io/file path)]
+      (if (fs/exists? file)
+        (str "file:" (.getCanonicalPath file))
+        (throw (AssertionError. (str "npm: bad dependency " version)))))))
+
+(defn- fixup-paths [deps]
+  (for [[name version] deps]
+    [name (or (translate-version version) version)]))
 
 (defn emit-json [{{bin :bin} :nodecljs :keys [name description version url npm]} workdir]
   (let [path (io/file workdir "package.json")
@@ -10,6 +22,7 @@
               :homepage url
               :dependencies (->> npm
                                  :dependencies
+                                 fixup-paths
                                  (into (sorted-map)))
               :bin {(or bin name) "./main.js"}}
         content (json/generate-string json {:pretty true})]
