1a18fd42f5e282ca74975603fec91ebb8e3e2af1
Greg Haskins <gregory.haskins@gmail.com>
2017-09-27 14:36:59 -0400

    Update comments
    Signed-off-by: Greg Haskins <gregory.haskins@gmail.com>

--- plugin/src/lein_nodecljs/package.clj
+++ plugin/src/lein_nodecljs/package.clj
@@ -3,6 +3,22 @@
             [me.raynes.fs :as fs]
             [cheshire.core :as json]))
 
+;;------------------------------------------------------------------------------
+;; translate-version: this function takes a "version" string as input (from the
+;; second item in an entry from the :npm { :dependencies []} array) and
+;; determines if it happens to be a 'LocalPath' spec as defined:
+;;
+;;        https://docs.npmjs.com/files/package.json#local-paths
+;;
+;; If it is, it translates what is likely a relative path into an FQP such that
+;; it becomes location independent.  This is helpful since we typically
+;; synthesize the package.json in a path that differs from the location of
+;; the project.clj which defined it.
+;;
+;; N.B. file: and translated paths are not advised for use in an official
+;; release of your project if it will be pushed to NPM.  However, they are
+;; handy during development
+;;------------------------------------------------------------------------------
 (defn- translate-version [version]
   (when-let [[_ path] (re-matches #"file:(.*)" version)]
     (let [file (io/file path)]
@@ -10,10 +26,17 @@
         (str "file:" (.getCanonicalPath file))
         (throw (AssertionError. (str "npm: bad dependency " version)))))))
 
+;;------------------------------------------------------------------------------
+;; fixup-paths: run through all dependencies and translate any file: based
+;; entries
+;;------------------------------------------------------------------------------
 (defn- fixup-paths [deps]
   (for [[name version] deps]
     [name (or (translate-version version) version)]))
 
+;;------------------------------------------------------------------------------
+;; emit-json: synthesizes a "package.json" file from our project settings
+;;------------------------------------------------------------------------------
 (defn emit-json [{{bin :bin} :nodecljs :keys [name description version url npm]} workdir]
   (let [path (io/file workdir "package.json")
         json {:name name
