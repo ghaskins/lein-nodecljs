e40c48fc194197a696147ad289ba457a7179c460
Greg Haskins <gregory.haskins@gmail.com>
2017-10-06 23:04:13 -0400

    Release v0.11.1
    Changes since v0.11.0
    ----------------------
    557fa1d Accidentally disabled source-map output
    
    Signed-off-by: Greg Haskins <gregory.haskins@gmail.com>

--- plugin/project.clj
+++ plugin/project.clj
@@ -1,4 +1,4 @@
-(defproject lein-nodecljs "0.12.0-SNAPSHOT"
+(defproject lein-nodecljs "0.11.1"
   :description "A plugin for Leiningen to assist with building Clojurescript apps for NodeJS"
   :url "http://github.com/ghaskins/lein-nodecljs"
   :license {:name "Eclipse Public License"
